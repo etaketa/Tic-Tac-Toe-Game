688d5d39d0dfea37acb688f77d930c85b70f4ba1
jjmaitan <jjmaitan@colostate.edu>
2024-10-06 14:19:48 -0600

    Cleaned up the code, did not change any logic

--- src/client.py
+++ src/client.py
@@ -16,28 +16,34 @@ def connect_to_server(hostName, portNumber):
     return clientSocket
 
 
+def main():
+    if len(sys.argv) != 3:
+        print("usage:", sys.argv[0], "<host> <port>")
+        sys.exit(1)
 
-if len(sys.argv) != 3:
-    print("usage:", sys.argv[0], "<host> <port>")
-    sys.exit(1)
+    host, port = sys.argv[1], int(sys.argv[2])
+    clientSocket = connect_to_server(host, port)
+    clientIsPlaying = True
+    print(f"Successfully connected to '{socket.gethostbyaddr(host)[1][0]}'!\n")
 
-host, port = sys.argv[1], int(sys.argv[2])
-clientSocket = connect_to_server(host, port)
-clientIsPlaying = True
-print(f"Successfully connected to '{socket.gethostbyaddr(host)[1][0]}'!\n")
+    # TODO: this is where we should send a RegisterRequest from this Client to the server so the server can know things such as the client socket
 
-try:
-    while clientIsPlaying:  # while the client is connected to the server
-        receiveMessage = clientSocket.recv(1024).decode()
-        if receiveMessage == "Waiting for another client...":
-            print(str(receiveMessage))
-            print()
-        else:
-            print(str(receiveMessage))
-            message = input("Write your message: ")
-            clientSocket.send(message.encode())
-except KeyboardInterrupt:
-    print("Disconnecting from the server")
-finally:
-    clientIsPlaying = False
-    clientSocket.close()
+    try:
+        while clientIsPlaying:  # while the client is connected to the server
+            # TODO: While the client is connected to the user, they could ask to see a list of other nodes in the system, invite a particular node, and play a game, or leave the server
+            receiveMessage = clientSocket.recv(1024).decode()
+            if receiveMessage == "Waiting for another client...":
+                print(str(receiveMessage))
+                print()
+            else:
+                print(str(receiveMessage))
+                message = input("Write your message: ")
+                clientSocket.send(message.encode())
+    except KeyboardInterrupt:
+        print("Disconnecting from the server")
+    finally:
+        clientIsPlaying = False
+        clientSocket.close()
+
+if __name__ == "__main__":
+    main()
