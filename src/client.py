ddd6a5afb19708d0b956d17f8920ef20d3557e69
jjmaitan <jjmaitan@colostate.edu>
2024-10-14 14:31:04 -0600

    insanity

--- src/client.py
+++ src/client.py
@@ -17,13 +17,13 @@ def connect_to_server(hostName, portNumber):
 
 
 def create_request(requestType):
-    if requestType == 0:  # Send a register request to the server
+    if requestType == 1:  # Send a register request to the server
         return dict()
-    elif requestType == 1:  # Send a deregister request to the server
+    elif requestType == 2 or requestType == 5:  # Send a deregister request to the server/Exit the client.
         return dict()
-    elif requestType == 2:  # Send a request to see all peer nodes in the system
+    elif requestType == 3:  # Send a request to see all peer nodes in the system
         return dict()
-    elif requestType == 3:  # Send an invite to another client to the server
+    elif requestType == 4:  # Send an invite to another client to the server
         return dict()
     
 
@@ -32,32 +32,35 @@ def on_request_recieved(request):
 
 
 def get_user_input():
-    print("1. Register")
-    print("2. Deregister")
+    print("1. Register to the server")
+    print("2. Deregister from the server")
     print("3. List other players")
     print("4. Invite a player")
     print("5. Exit")
     user_input = input("Enter a number to select an option: ")
     return user_input
 
+def main():
+    if len(sys.argv) != 3:
+        print("usage:", sys.argv[0], "<host> <port>")
+        sys.exit(1)
 
-if len(sys.argv) != 3:
-    print("usage:", sys.argv[0], "<host> <port>")
-    sys.exit(1)
+    host, port = sys.argv[1], int(sys.argv[2])
+    clientSocket = connect_to_server(host, port)
+    clientIsConnected = True
+    print(f"[{clientSocket.gethostname}] successfully connected to Server @ '{socket.gethostbyaddr(host)[1][0]}'!\n")
 
-host, port = sys.argv[1], int(sys.argv[2])
-clientSocket = connect_to_server(host, port)
-clientIsConnected = True
-print(f"Successfully connected to '{socket.gethostbyaddr(host)[1][0]}'!\n")
+    try:
+        while clientIsConnected:  # while the client is connected to the server
+            message = create_request(get_user_input())
+            receiveMessage = clientSocket.recv(1024).decode()
+            clientSocket.send(message.encode())
+            # on recieving a response from the server call the on_request_recieved function
+    except KeyboardInterrupt:
+        print("Disconnecting from the server")
+    finally:
+        clientIsConnected = False
+        clientSocket.close()
 
-try:
-    while clientIsConnected:  # while the client is connected to the server
-        message = create_request(get_user_input())
-        receiveMessage = clientSocket.recv(1024).decode()
-        clientSocket.send(message.encode())
-        # on recieving a response from the server call the on_request_recieved function
-except KeyboardInterrupt:
-    print("Disconnecting from the server")
-finally:
-    clientIsConnected = False
-    clientSocket.close()
+if __name__ == "__main__":
+    main() 
--- src/server.py
+++ src/server.py
@@ -13,45 +13,50 @@ def start_connections():
     print(f"Successfully connected to: {str(address)}")
     return client_connection
 
-if len(sys.argv) != 2:
-    print(f"usage = {sys.argv[0]} <portNumber>")
-    sys.exit(1)
 
-host = socket.gethostname()
-portNumber = int(sys.argv[1])
+def main():
+    if len(sys.argv) != 2:
+        print(f"usage = {sys.argv[0]} <portNumber>")
+        sys.exit(1)
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    portNumber = int(sys.argv[1])
 
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serverSocket.bind((host, portNumber))
-serverSocket.listen(2) #only allow 2 clients to connect to server
-print(f"Server is running and listening on {(host, portNumber)}")
-serverIsRunning = True
-serverSocket.setblocking(False)
-sel.register(serverSocket, selectors.EVENT_READ, data=None)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    client_connection1 = start_connections()
-    message = "Waiting for another client..."
-    client_connection1.send(message.encode())
-    client_connection2 = start_connections()
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind((host, portNumber))
+    serverSocket.listen(2) #only allow 2 clients to connect to server
+    print(f"[Server] is running and listening on {(host, portNumber)}")
+    serverIsRunning = True
+    serverSocket.setblocking(False)
+    sel.register(serverSocket, selectors.EVENT_READ, data=None)
 
-    message = "Both clients are connected!"
-    client_connection1.send(message.encode())
+    try:
+        client_connection1 = start_connections()
+        message = "Waiting for another client..."
+        client_connection1.send(message.encode())
+        client_connection2 = start_connections()
 
-    while serverIsRunning:  # while the server is running
-
-        client_message1 = client_connection1.recv(1024).decode()
-        message = "Received from client 1: " + str(client_message1)
-        
-        client_connection2.send(message.encode())
-        client_message2 = client_connection2.recv(1024).decode()
-        message = "Received from client 2: " + str(client_message2)
+        message = "Both clients are connected!"
         client_connection1.send(message.encode())
 
-except KeyboardInterrupt:
-    serverIsRunning = False
-    print("Exiting")
-finally:
-    client_connection1.close()
-    client_connection2.close()
+        while serverIsRunning:  # while the server is running
+
+            client_message1 = client_connection1.recv(1024).decode()
+            message = "Received from client 1: " + str(client_message1)
+            
+            client_connection2.send(message.encode())
+            client_message2 = client_connection2.recv(1024).decode()
+            message = "Received from client 2: " + str(client_message2)
+            client_connection1.send(message.encode())
+
+    except KeyboardInterrupt:
+        serverIsRunning = False
+        print("Exiting")
+    finally:
+        client_connection1.close()
+        client_connection2.close()
+
+if __name__ == "__main__":
+    main()
