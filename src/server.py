6afe5a16f9797320043fc4dd41ff72eca84a0ba1
jjmaitan <jjmaitan@colostate.edu>
2024-10-16 13:57:16 -0600

    Adding base changes

--- src/server.py
+++ src/server.py
@@ -7,12 +7,35 @@ sel = selectors.DefaultSelector()
 
 def start_connections():
     events = sel.select(timeout=None)
-    key = events[0]
-    sock = key[0].fileobj
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            service_connection(key, mask)
+
+def accept_wrapper(sock):
     client_connection, address = sock.accept()
     print(f"Successfully connected to: {str(address)}")
-    return client_connection
+    client_connection.setblocking(False)
+    sel.register(client_connection, selectors.EVENT_READ, data=None)
 
+def service_connection(key, mask):
+    sock = key.fileobj
+    if mask & selectors.EVENT_READ:
+        data = sock.recv(1024)  # Adjust buffer size as needed
+        if data:
+            handle_request(data, sock)
+        else:
+            sel.unregister(sock)
+            sock.close()
+
+def handle_request(request=None, sock=None):
+    print(request)
+    # unpack the request
+    # check the value of the request
+    # send the appropriate response
+    response = b"Response from server"  # Example response
+    sock.send(response)  # create appropriate response for the user
 
 def main():
     if len(sys.argv) != 2:
@@ -23,46 +46,20 @@ def main():
     portNumber = int(sys.argv[1])
 
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serverSocket.bind((host, portNumber))
-    serverSocket.listen(2) #only allow 2 clients to connect to server
+    serverSocket.listen(2)  # only allow 2 clients to connect to server
     print(f"[Server] is running and listening on {(host, portNumber)}")
-    serverIsRunning = True
     serverSocket.setblocking(False)
     sel.register(serverSocket, selectors.EVENT_READ, data=None)
 
-    connected_clients = [] # list of all the connected clients
-
     try:
-        while serverIsRunning:  # while the server is running
-            connected_clients.append(start_connections())
-        # client_connection1 = start_connections()
-        # message = "Waiting for another client..."
-        # client_connection1.send(message.encode())
-        # client_connection2 = start_connections()
-
-        # message = "Both clients are connected!"
-        # client_connection1.send(message.encode())
-
-        
-
-            # client_message1 = client_connection1.recv(1024).decode()
-            # message = "Received from client 1: " + str(client_message1)
-            
-            # client_connection2.send(message.encode())
-            # client_message2 = client_connection2.recv(1024).decode()
-            # message = "Received from client 2: " + str(client_message2)
-            # client_connection1.send(message.encode())
-
+        while True:  # while the server is running
+            start_connections()
     except KeyboardInterrupt:
-        serverIsRunning = False
-        print("Exiting")
+        print("Server is shutting down.")
     finally:
-        for client in connected_clients:
-            client.close()
-
-        serverSocket.close()
+        sel.close()
 
 if __name__ == "__main__":
     main()
