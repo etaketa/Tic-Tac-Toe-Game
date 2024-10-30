40bd55f38e2ea3b217d229d6a576cd8d5c9b93ca
Erin Taketa <erin.ktaketa@gmail.com>
2024-10-30 17:57:53 -0600

    Put in sprint 3 requirements

--- client.py
+++ client.py
@@ -1,86 +1,55 @@
 import sys
 import socket
 import selectors
-import logging
+import traceback
+import json
+import server
 import threading
-import clientmsg
-
 
 sel = selectors.DefaultSelector()
+clientIsConnected = False
+
+def connect_to_server(hostName, portNumber):
+    address = (hostName, portNumber)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect_ex(address)  # connect to the server without blocking the main thread
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(clientSocket, events, data=None)
+    return clientSocket
 
 
-CLIENT_IS_CONNECTED = False
-
-
-def create_request(requestType):
-    logging.info(f"client.py - Creating request type {requestType}")
-    print(f"Creating request type {requestType}")
-
-    if requestType == 1:  # Send an invite request to the server
-        return dict(
-            type="text/json",
-            encoding="utf-8",
-            content=dict(action=None, value=requestType),
-        )
-    elif requestType == 2:
-        return dict(
-            type="text/json",
-            encoding="utf-8",
-            content=dict(action=None, value=requestType),
-        )
+def create_request(requestType, username):
+    if username or dataIsValid(requestType):
+        request = requestType
+        return b"%s" % request.encode()
     else:
         print("Invalid request type")
         return None
 
+def dataIsValid(data):
+    valid_input = ['a','b','c','d','e','f','g','h','i']
+    for character in valid_input:
+        if character == data:
+            return True
+    return False
 
-def get_user_input():
-    logging.info("client.py - Getting user input")
-    print("1. Invite a player")  # maybe list all players and ask for the player to invite
-    print("2. Exit")
-    user_input = input("Enter a number to select an option: ")
-    return user_input
-
-
-# This is a function handled in a seperate thread
 def handle_server_communication(clientSocket, address):
     try:
         while True:
-            events = sel.select(timeout=1)
-            for key, mask in events:
-                message = key.data
-                try:
-                    message.process_events(mask)
-                except Exception:
-                    message.close()
-            if not sel.get_map():
-                break
+            try:
+                data = clientSocket.recv(1024)
+                if "cancelling" in data.decode():
+                    print("Disconnecting from the server")
+                    sys.exit(0)
+                if data:
+                    print(data.decode())
+            except Exception as e:
+                clientSocket.close()
     except KeyboardInterrupt:
-        logging.info("Caught keyboard interrupt, disconnecting from the server")
         print("Disconnecting from the server")
+        sys.exit(0)
     finally:
-        logging.info("Closing connection to the server and closing client socket")
         clientSocket.close()
-
-
-# This is a function handled in a seperate thread
-def handle_user_input(clientSocket, address):
-    while CLIENT_IS_CONNECTED:
-        user_input = get_user_input()
-        request = create_request(int(user_input))
-
-        if request:
-            message = clientmsg.Message(sel, clientSocket, address, request)
-            events = selectors.EVENT_READ | selectors.EVENT_WRITE
-            sel.modify(clientSocket, events, data=message)
-
-
-def connect_to_server(host, port):
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((host, port))
-    clientSocket.setblocking(False)
-    sel.register(clientSocket, selectors.EVENT_READ, data=None)
-    return clientSocket, clientSocket.getsockname()
-
 
 def main():
     if len(sys.argv) != 3:
@@ -88,44 +57,26 @@ def main():
         sys.exit(1)
 
     host, port = sys.argv[1], int(sys.argv[2])
-    clientSocket, address = connect_to_server(host, port)
-    logging.info(f"[{socket.gethostname()}] successfully connected to Server")
-    CLIENT_IS_CONNECTED = True
+    clientSocket = connect_to_server(host, port)
+    clientIsConnected = True
+    address = 1
     print(f"[{socket.gethostname()}] successfully connected to Server")
 
-    try:
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        request = create_request(int(get_user_input()))
-        message = clientmsg.Message(sel, clientSocket, address, request)
-        sel.modify(clientSocket, events, data=message)
-        
-        while CLIENT_IS_CONNECTED:  # while the client is connected to the server
-            events = sel.select(timeout=1)
-            for key, mask in events:
-                message = key.data
-                try:
-                    message.process_events(mask)
-                except Exception:
-                    message.close()
-            if not sel.get_map():
-                break
-    except KeyboardInterrupt:
-        logging.info("Caught keyboard interrupt, disconnecting from the server")
-        print("Disconnecting from the server")
-    finally:
-        logging.info("Closing connection to the server and closing client socket")
-        CLIENT_IS_CONNECTED = False
-        clientSocket.close()
-
-    # Start threads for server communication and user input
-    # server_thread = threading.Thread(target=handle_server_communication, args=(clientSocket, address))
-    user_input_thread = threading.Thread(target=handle_user_input, args=(clientSocket, address), daemon=True)  # Takes it its own clientSocket and address (IP, port), daemon=True means when the main thread stops executing so does this thread
-
-    #server_thread.start()
+    user_input_thread = threading.Thread(target=handle_server_communication, args=(clientSocket, address), daemon=True)
     user_input_thread.start()
 
-    #server_thread.join()
-    user_input_thread.join()
+    try:
+        message = create_request(input("Enter desired username: "), True)
+        clientSocket.send(message)
+        while clientIsConnected:
+            message = create_request(input(), False)
+            if message:
+                clientSocket.send(message)
+    except KeyboardInterrupt:
+        print("Disconnecting from the server")
+    finally:
+        clientIsConnected = False
+        sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    main() 
